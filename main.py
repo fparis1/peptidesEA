aa2d5e694b349a2156be5ab642bdb3de6e0d91c3
Filip Paris <parisfilip2@gmail.com>
2024-05-21 18:45:41 +0200

    changes regarding constants used in fitness calculation

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 from genetic_algorithm import GeneticAlgorithm
 
 if __name__ == "__main__":
-    target_molecular_weight = 1500
-    target_isoelectric_point = 10.0
-    target_hydrophobicity = 0.5
-    target_charge = 5
-    target_aliphatic_index = 50
-    target_instability_index = 40
-    target_boman = 2
-    target_hydrophobic_moment = 0.5
-    target_mass_to_charge_ratio = 800
+    # Define the target values
+    target_molecular_weight = 3000  # Target molecular weight (around 3 kDa)
+    target_isoelectric_point = 9.5  # High isoelectric point
+    target_hydrophobicity = 0.4  # Moderate hydrophobicity
+    target_charge = 5  # Positive net charge
+    target_aliphatic_index = 90  # High aliphatic index
+    target_instability_index = 40  # Low instability index
+    target_boman = 1.0  # Boman index around 1.0
+    target_hydrophobic_moment = 0.6  # Hydrophobic moment around 0.6
 
+    # Parameters for the genetic algorithm
     population_size = 100
     offspring_size = 100
     num_generations = 100
     mutation_probability = 0.1
 
+    # Initialize the genetic algorithm with the target values
     genetic_algorithm = GeneticAlgorithm(target_molecular_weight, target_isoelectric_point, target_hydrophobicity,
                                          target_charge, target_aliphatic_index, target_instability_index,
-                                         target_boman, target_hydrophobic_moment, target_mass_to_charge_ratio,
+                                         target_boman, target_hydrophobic_moment,
                                          population_size, offspring_size, num_generations, mutation_probability)
-    best_sequence, molecular_weight, isoelectric_point, hydrophobicity, charge, aliphatic_index, instability_index, \
-        boman, hydrophobic_moment, mass_to_charge_ratio = genetic_algorithm.optimize()
 
+    # Run the optimization
+    best_sequence, molecular_weight, isoelectric_point, hydrophobicity, charge, aliphatic_index, instability_index, \
+        boman, hydrophobic_moment = genetic_algorithm.optimize()
+
+    # Print the optimized peptide properties
     print("\nOptimized Peptide:")
     print("Sequence:", best_sequence)
     print("Molecular Weight:", molecular_weight)
@@ -33,5 +38,3 @@ if __name__ == "__main__":
     print("Instability Index:", instability_index)
     print("Boman:", boman)
     print("Hydrophobic Moment:", hydrophobic_moment)
-    print("Mass-to-Charge Ratio:", mass_to_charge_ratio)
-
